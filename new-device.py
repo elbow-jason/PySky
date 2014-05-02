503955d6b046c36f5b983515ef0b0870649c4a24
jlgoldb2 <jlgoldb2@asu.edu>
2014-05-02 12:36:18 -0700

    refactored new-device.py

--- new-device.py
+++ new-device.py
@@ -1,17 +1,23 @@
 import urllib
 import pycurl
+import StringIO
 
-def getNewID():
-  curler = pycurl.Curl()
-  req_type = {'type' :' example'}
+def new-device():
+#set up vars
+  id_holder = StringIO.StringIO()
+  curler    = pycurl.Curl()
+  req_type  = {'type' :' example'}
+  post      = urllib.urlencode(req_type)
+
+#set up curler
   curler.setopt(curler.URL, 'http://skynet.im/devices')
   curler.setopt(curler.POST, 1)
-  post = urllib.urlencode(req_type)
+  curler.setopt(curler.WRITEFUNCTION, id_holder.write)
   curler.setopt(curler.POSTFIELDS, post)
-
   curler.perform()
 
-  print "retrieved: \n", curler
-  return curler
+  print "id_holder.getvalue() =>"
+  print id_holder.getvalue()
+  return id_holder.getvalue()
 
-new_device = getNewID()
+skyID  = getNewID();
